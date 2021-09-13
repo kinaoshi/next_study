2b73a4ee026a7c5eb878181a4f1b1e6f03e26bfc
kinaoshi <ku.928.maid@gmail.com>
2021-09-13 20:37:19 +0900

    ✨ useState で文字列を真偽値について解説

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,42 +12,55 @@ import styles from "../styles/Home.module.css";
 // }
 
 export default function Home() {
-  const [count, setCount] = useState(1);
+	const [count, setCount] = useState(1);
+	const [text, setText] = useState("");
+	const [isShow, setIsShow] = useState(true);
 
-  const handleClick = useCallback((e) => {
-    if (count < 10) {
+	const handleClick = useCallback(
+		(e) => {
+			if (count < 10) {
+				setCount((count) => count + 1);
+			}
+		},
+		[count]
+	);
 
-      setCount ((count) => count + 1);
-    }
-    },[count]);
+	useEffect(() => {
+		document.body.style.backgroundColor = "lightblue";
+		return () => {
+			document.body.style.backgroundColor = "";
+		};
+	}, [count]);
 
-  useEffect(() => {
-    console.log(`マウント時:${count}`);
-    document.body.style.backgroundColor = "lightblue";
-    return () => {
-      console.log(`アンマウント時:${count}`);
-      document.body.style.backgroundColor = "";
-    }
-  },[count]);
+	const handleChange = useCallback((e) => {
+		if (e.target.value.length >= 5) {
+			alert("5文字以内にして下さい");
+			return;
+		}
+		setText(e.target.value.trim());
+	}, []);
+	console.log(text);
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  },[])
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Index page </title>
+			</Head>
+			<Header />
+			{isShow ? <h1>{count}</h1> : null}
+			<button onClick={handleClick}>ボタン</button>
+			<button
+				onClick={handleDisplay}
+			>
+				{isShow ? "非表示" : "表示"}
+			</button>
+			<input type="text" value={text} onChange={handleChange} />
 
-  const handleOnClick = useCallback(() => {
-    alert("次は12からです")
-  },[]);
-  
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Index page </title>
-      </Head>
-      <Header />
-      <h1>{count}</h1>
-      <button onClick={handleClick}>
-        ボタン
-      </button>
-    <button onClick = {handleOnClick}>学習状況</button>
-      <Main page="index" />
+			<Main page="index" />
 
-      <Footer />
-    </div>
-  );
+			<Footer />
+		</div>
+	);
 }
